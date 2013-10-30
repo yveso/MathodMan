53693752203a466f66be906a63cbaf46f5a1db5a
yveso <yves.oehlschlaeger@googlemail.com>
2013-10-30 21:02:28 +0100

    Changed Mode Class Data Classes to Shortcutsyntax

--- MathodMan.UnitTests/Statistics/TheoryClassData/ModeClassData.cs
+++ MathodMan.UnitTests/Statistics/TheoryClassData/ModeClassData.cs
@@ -7,11 +7,11 @@ namespace MathodMan.UnitTests.Statistics.TheoryClassData
         public ModeClassDataObject()
             : base(new List<object[]>
             {
-                new object[] { new List<string> { "a" }, new List<string> { "a" } },
-                new object[] { new List<string> { "a", "b", "c" }, new List<string> { "a", "b", "c" } },
-                new object[] { new List<string> { "a", "b", "c", "a" }, new List<string> { "a" } },
-                new object[] { new List<string> { "a", "b", "c", "a", "c" }, new List<string> { "a", "c" } },
-                new object[] { new List<string> { "a", "b", "c", "a", "c" }, new List<string> { "c", "a" } }
+                new [] { new List<string> { "a" }, new List<string> { "a" } },
+                new [] { new List<string> { "a", "b", "c" }, new List<string> { "a", "b", "c" } },
+                new [] { new List<string> { "a", "b", "c", "a" }, new List<string> { "a" } },
+                new [] { new List<string> { "a", "b", "c", "a", "c" }, new List<string> { "a", "c" } },
+                new [] { new List<string> { "a", "b", "c", "a", "c" }, new List<string> { "c", "a" } }
             })
         {
         }
@@ -22,11 +22,11 @@ namespace MathodMan.UnitTests.Statistics.TheoryClassData
         public ModeClassDataInt()
             :base(new List<object[]> 
             {
-                new object[] { new List<int> { 4 }, new List<int> { 4 } },
-                new object[] { new List<int> { 4, 3, 7 }, new List<int> { 4, 3, 7 } },
-                new object[] { new List<int> { 4, 3, 7, 4, 4, 2, 7 }, new List<int> { 4 } },
-                new object[] { new List<int> { 4, 3, 7, 4, 4, 2, 7, 7 }, new List<int> { 4, 7 } },
-                new object[] { new List<int> { 4, 3, 7, 4, 4, 2, 7, 7 }, new List<int> { 7, 4 } }
+                new [] { new List<int> { 4 }, new List<int> { 4 } },
+                new [] { new List<int> { 4, 3, 7 }, new List<int> { 4, 3, 7 } },
+                new [] { new List<int> { 4, 3, 7, 4, 4, 2, 7 }, new List<int> { 4 } },
+                new [] { new List<int> { 4, 3, 7, 4, 4, 2, 7, 7 }, new List<int> { 4, 7 } },
+                new [] { new List<int> { 4, 3, 7, 4, 4, 2, 7, 7 }, new List<int> { 7, 4 } }
             })
         {
         }
